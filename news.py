7551344d3fa9025e02c0befd56f14263385f7de8
vagrant <vagrant@vagrant.vm>
2017-11-22 17:34:37 +0000

    Add select statement to first question

--- news.py
+++ news.py
@@ -13,15 +13,20 @@ def connect():
 def create_views():
     conn, cur = connect()
     pass
-    cur.execute('create view ____ as')
+    cur.execute('''
+    create view articleviews as
+    ''')
     conn.commit()
     conn.close()
 
 
 def first_question():
     conn, cur = connect()
-    cur.execute("select * from authors")
+    cur.execute('''select articles.title, count(log.path) as views
+    from articles, log where status = '200 OK' and log.path like concat('%',articles.slug,'%')
+    group by articles.title order by views desc;
+    ''')
     question_one = cur.fetchall()[3][0]
-    print (question_one)
+    print ("The three most popular articles of all time are:" + question_one[1])
 
 first_question()
