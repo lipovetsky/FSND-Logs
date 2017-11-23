5304dfd13d548cd9d4f85d1464b5554e611be7d3
vagrant <vagrant@vagrant.vm>
2017-11-23 16:55:20 +0000

    Add view to answer second question

--- news.py
+++ news.py
@@ -12,26 +12,57 @@ def connect():
 
 def create_views():
     conn, cur = connect()
-    pass
-    cur.execute('''
-    create view articleviews as
-    ''')
-    conn.commit()
-    conn.close()
+    try:
+        cur.execute('''
+        create view articleviews as
+        select articles.title,
+        count(log.path) as views
+        from articles, log where status = '200 OK'
+        and log.path like concat('%',articles.slug,'%')
+        group by articles.title order by views desc;
+        ''')
+        conn.commit()
+        conn.close()
+    except psycopg2.Error:
+        pass
 
+    try:
+        cur.execute('''
+        create view popularauthors as
+        select authors.name,
+        count(log.path) as views
+        from articles, authors, log
+        where articles.author = authors.id
+        and status = '200 OK'
+        and log.path like concat('%',articles.slug,'%')
+        and articles.author = authors.id
+        group by authors.id
+        order by views desc;
+        ''')
+        conn.commit()
+        conn.close()
+    except psycopg2.Error:
+        pass
 
 def first_question():
     conn, cur = connect()
-    cur.execute('''select articles.title, count(log.path) as views
-    from articles, log where status = '200 OK' and log.path like concat('%',articles.slug,'%')
-    group by articles.title order by views desc;
-    ''')
+    cur.execute('select * from articleviews;')
     question_one = cur.fetchall()
-    print ("""The three most popular articles of all time are: \n
+    print ("""\nThe three most popular articles of all time are: \n
     1. {} - {} views \n
     2. {} - {} views \n
     3. {} - {} views \n""".format(question_one[0][0], question_one[0][1],
     question_one[1][0], question_one[1][1],
     question_one[2][0], question_one[2][1]))
 
+def second_question():
+    conn, cur = connect()
+    cur.execute('''select articleviews.*
+    from articleviews join authors where authors.id = articles.author
+    group by articles.author;
+    ''')
+    question_two = cur.fetchall()
+
+
+create_views()
 first_question()
