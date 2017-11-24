f2f9d1f205343284a3e5bd0442f8c0ee7baa8330
vagrant <vagrant@vagrant.vm>
2017-11-24 15:43:01 +0000

    Remove views for first and second question

--- news.py
+++ news.py
@@ -10,61 +10,67 @@ def connect():
     except:
         "Could not connect."
 
-def create_views():
-    try:
-        conn, cur = connect()
-        cur.execute('''
-        create view articleviews as
-        select articles.title,
-        count(log.path) as views
-        from articles, log
-        where status = '200 OK'
-        and log.path like concat('%',articles.slug,'%')
-        group by articles.title
-        order by views desc;
-        ''')
-        conn.commit()
-        conn.close()
-    except psycopg2.Error:
-        pass
+# def create_views():
+    # try:
+    #     conn, cur = connect()
+    #     cur.execute('''
+    #     create view okstatus as
+    #     select count(a.status),
+    #     count(b.status), a.time::DATE as date
+    #     from log as a, log as b
+    #     where a.time = b.time
+    #     where a.status != '200 OK'
+    #     and b.status != '200 OK'
+    #     group by date;
+    #     ''')
+    #     cur.execute('''
+    #     create view badstatus as
+    #     select count(a.status) as count1, count(b.status) as count2,
+    #     a.time::DATE as date
+    #     from log a, log b
+    #     where a.status = b.status
+    #     group by date
+    #     limit 50;
+    #     ''')
+    #     conn.commit()
+    #     conn.close()
+    # except psycopg2.Error:
+    #     pass
 
-    try:
-        conn, cur = connect()
-        cur.execute('''
-        create view popularauthors as
-        select authors.name,
-        count(log.path) as views
-        from articles, authors, log
-        where articles.author = authors.id
-        and status = '200 OK'
-        and log.path like concat('%',articles.slug,'%')
-        and articles.author = authors.id
-        group by authors.id
-        order by views desc;
-        ''')
-        conn.commit()
-        conn.close()
-    except psycopg2.Error:
-        pass
 
 def first_question():
     conn, cur = connect()
-    cur.execute('select * from articleviews;')
+    cur.execute('''select articles.title,
+    count(log.path) as views
+    from articles, log
+    where status = '200 OK'
+    and log.path like concat('%',articles.slug,'%')
+    group by articles.title
+    order by views desc;
+    ''')
     question_one = cur.fetchall()
     print ("""\nThe three most popular articles of all time are: \n
-    1. {} - {} views \n
-    2. {} - {} views \n
-    3. {} - {} views \n""".format(question_one[0][0], question_one[0][1],
+    1. "{}" - {} views \n
+    2. "{}" - {} views \n
+    3. "{}" - {} views \n""".format(question_one[0][0], question_one[0][1],
     question_one[1][0], question_one[1][1],
     question_one[2][0], question_one[2][1]))
     conn.close()
 
 def second_question():
     conn, cur = connect()
-    cur.execute('''select * from popularauthors;
+    cur.execute('''select authors.name,
+    count(log.path) as views
+    from articles, authors, log
+    where articles.author = authors.id
+    and status = '200 OK'
+    and log.path like concat('%',articles.slug,'%')
+    and articles.author = authors.id
+    group by authors.id
+    order by views desc;
     ''')
     question_two = cur.fetchall()
-    print ("""\nThe most popular authors are are: \n
+    print ("""The most popular authors are: \n
     1. {} - {} views \n
     2. {} - {} views \n
     3. {} - {} views \n
@@ -74,6 +80,18 @@ def second_question():
     question_two[3][0], question_two[3][1]))
     conn.close()
 
-create_views()
+def third_question():
+    conn, cur = connect()
+    cur.execute('''
+    select a.time, a.status, b.time, b.status
+    from log as a, log as b
+    where a.status != '200 OK'
+    and a.status = b.status
+    group by a.time, b.time, a.status, b.status,
+    order by a.time;
+    ''')
+    conn.close()
+
+# create_views()
 first_question()
 second_question()
